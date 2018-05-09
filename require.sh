e0fb52f0fe8869b304d5020a50a50526c20722b9
mozgbrasil <mozgbrasil@gmail.com>
2018-05-09 14:23:19 -0300

    .

--- require.sh
+++ require.sh
@@ -12,33 +12,33 @@ docker-php-ext-install opcache gd bcmath intl mbstring mcrypt pdo_mysql soap xsl
 
 # Install oAuth
 
-#apt-get install -y libpcre3 libpcre3-dev && pecl install oauth && echo "extension=oauth.so" > /usr/local/etc/php/conf.d/docker-php-ext-oauth.ini
+apt-get install -y libpcre3 libpcre3-dev && pecl install oauth && echo "extension=oauth.so" > /usr/local/etc/php/conf.d/docker-php-ext-oauth.ini
 
 # Install Node, NVM, NPM and Grunt
 
-#curl -sL https://deb.nodesource.com/setup_6.x | bash - && apt-get install -y nodejs build-essential && curl https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | sh && npm i -g grunt-cli yarn
+curl -sL https://deb.nodesource.com/setup_6.x | bash - && apt-get install -y nodejs build-essential && curl https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | sh && npm i -g grunt-cli yarn
 
 # Install Composer
 
-#curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin/ --filename=composer
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin/ --filename=composer
 
-#composer global require hirak/prestissimo
+composer global require hirak/prestissimo
 
 # Install XDebug
 
-#yes | pecl install xdebug && echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.iniOLD
+yes | pecl install xdebug && echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.iniOLD
 
 # Install Mhsendmail
 
-#DEBIAN_FRONTEND=noninteractive apt-get -y install golang-go && mkdir /opt/go && export GOPATH=/opt/go && go get github.com/mailhog/mhsendmail 
+DEBIAN_FRONTEND=noninteractive apt-get -y install golang-go && mkdir /opt/go && export GOPATH=/opt/go && go get github.com/mailhog/mhsendmail 
 
 # Install Magerun 2
 
-#wget https://files.magerun.net/n98-magerun2.phar && chmod +x ./n98-magerun2.phar && mv ./n98-magerun2.phar /usr/local/bin/
+wget https://files.magerun.net/n98-magerun2.phar && chmod +x ./n98-magerun2.phar && mv ./n98-magerun2.phar /usr/local/bin/
 
 # Install ioncube loader
 
-#curl -fsSL 'http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz' -o ioncube.tar.gz && mkdir -p ioncube && tar -xf ioncube.tar.gz -C ioncube --strip-components=1 && rm ioncube.tar.gz && mv ioncube/ioncube_loader_lin_7.0.so /var/www/ioncube_loader_lin_7.0.so && rm -r ioncube 
+curl -fsSL 'http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz' -o ioncube.tar.gz && mkdir -p ioncube && tar -xf ioncube.tar.gz -C ioncube --strip-components=1 && rm ioncube.tar.gz && mv ioncube/ioncube_loader_lin_7.0.so /var/www/ioncube_loader_lin_7.0.so && rm -r ioncube 
 
 #
 
